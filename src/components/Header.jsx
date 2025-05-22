50219b5391d981edc6b358a5a7e3bb244081944b
Andres Holguin <andresmholguin@gmail.com>
2025-05-21 19:41:30 -0500

    Mejorar estilos en el componente App y ajustar el diseño del formulario en el componente Header para una mejor usabilidad.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,15 +47,16 @@ const Header = ({ guardarTarea, editando, setEditando }) => {
   return (
     <div className="flex flex-col mb-12">
       <h1 className="text-center text-4xl font-bold">TO DO LIST</h1>
-      <form action="" className="flex justify-between w-[100%] gap-2 mt-4">
+      <form action="" className="relative flex justify-between gap-2 mt-4">
         <input
-          className="bg-Dark-700 h-12 w-[100%] rounded-lg p-4 outline-none shadow-xl/30 focus:shadow-xl/30 focus:shadow-Dark-400/70 transition-all duration-200"
+          className="bg-Dark-700 h-12 w-[100%] rounded-lg p-4 outline-none shadow-xl/30 focus:shadow-xl/50 focus:shadow-Dark-400/70 transition-all duration-200"
           id="input"
           type="text"
           placeholder="Ejercicio, Estudiar, Compras"
         />
+
         <button
-          className="bg-Dark-100 px-6 w-[180px] rounded-lg shadow-md cursor-pointer text-Dark-900 font-bold hover:bg-Dark-400 hover:text-white active:bg-Dark-400/40 transition-all duration-300"
+          className="absolute h-[48px] w-[120px] right-0 bg-Dark-100 px-6  rounded-r-lg shadow-md cursor-pointer text-Dark-900 font-bold hover:bg-Dark-400 hover:text-white active:bg-Dark-400/40 transition-all duration-300"
           id="btnSubmit"
           type="submit"
           onClick={saveStorage}
