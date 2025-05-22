270c3491ac2d9a8fa4407d697b6378931403f293
Andres Holguin <andresmholguin@gmail.com>
2025-05-21 19:46:48 -0500

    Agregar manejo de estado para mostrar mensaje cuando no hay tareas en el componente Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,29 +15,33 @@ const Main = ({ tareas, eliminarTarea, setEditando }) => {
     <div>
       <h1 className="text-2xl font-bold mb-4">Tareas</h1>
       <ul>
-        {tareas.map((tarea, index) => (
-          <li
-            key={index}
-            className="flex justify-between items-center bg-Dark-800 p-3 mb-2 rounded-lg transition-all duration-300"
-          >
-            {tarea}
-            <div className="flex gap-2">
-              <button
-                onClick={() => editar(tarea, index)}
-                className="px-3 py-2 bg-Dark-100 cursor-pointer rounded-sm transition-all duration-300 hover:bg-amber-500"
-              >
-                <EditIcon />
-              </button>
-              {/* <Delete /> */}
-              <button
-                onClick={() => eliminar(tarea)}
-                className="px-3 py-2 bg-Dark-100 rounded-sm cursor-pointer hover:bg-red-700 transition-all duration-300"
-              >
-                <DeleteIcon />
-              </button>
-            </div>
-          </li>
-        ))}
+        {tareas.length === 0 ? (
+          <li className="text-center text-lg">No hay tareas</li>
+        ) : (
+          tareas.map((tarea, index) => (
+            <li
+              key={index}
+              className="flex justify-between items-center bg-Dark-800 p-3 mb-2 rounded-lg transition-all duration-300"
+            >
+              {tarea}
+              <div className="flex gap-2">
+                <button
+                  onClick={() => editar(tarea, index)}
+                  className="px-3 py-2 bg-Dark-100 cursor-pointer rounded-sm transition-all duration-300 hover:bg-amber-500"
+                >
+                  <EditIcon />
+                </button>
+                {/* <Delete /> */}
+                <button
+                  onClick={() => eliminar(tarea)}
+                  className="px-3 py-2 bg-Dark-100 rounded-sm cursor-pointer hover:bg-red-700 transition-all duration-300"
+                >
+                  <DeleteIcon />
+                </button>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
