40321c84e355d832d4f0fb1f92bb3d1bcfb0f0ee
Andres Holguin Work <andresmholguin@gmail.com>
2025-05-28 17:23:53 -0500

    Refactor fetchTareas para usar una variable intermedia userDataId al obtener tareas del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -86,11 +86,11 @@ function App() {
     }
 
     try {
-      console.log(userData.id);
+      const userDataId = userData.id;
       const { data, error } = await supabase
         .from("TodoList")
         .select("*")
-        .eq("userId", userData.id) // Tareas del usuario actual.
+        .eq("userId", userDataId) // Tareas del usuario actual.
         .eq("delete", false) // Tareas que no están eliminadas.
         .order("dateTask", { ascending: true });
 
