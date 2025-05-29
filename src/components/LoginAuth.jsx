33db7eaf479d29dac27b2df240db434dce291d9c
Andres Holguin <andresmholguin@gmail.com>
2025-05-28 23:10:45 -0500

    Estilos para el componente LoginAuth

--- src/components/LoginAuth.jsx
+++ src/components/LoginAuth.jsx
@@ -7,7 +7,7 @@ import {
 
 const LoginAuth = () => {
   return (
-    <div className="flex justify-center p-2 rounded-full bg-Dark-900">
+    <div className="flex justify-center p-2 ">
       <SignedOut>
         <SignInButton>
           <button className="px-2 bg-gray-950 text-white rounded-lg hover:bg-gray-900 hover:shadow-sm hover:shadow-amber-700 transition-all  cursor-pointer">
