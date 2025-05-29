0310485def8bde58f25b5781260dc95172b1465f
Andres Holguin <andresmholguin@gmail.com>
2025-05-28 23:47:24 -0500

    Actualizar alineación y altura de elementos en LoginAuth

--- src/components/LoginAuth.jsx
+++ src/components/LoginAuth.jsx
@@ -7,17 +7,17 @@ import {
 
 const LoginAuth = () => {
   return (
-    <div className="flex justify-center p-2 ">
+    <div className="flex justify-center items-center p-2 ">
       <SignedOut>
         <SignInButton>
-          <button className="px-2 hover:bg-gray-950 text-white rounded-lg bg-gray-900 shadow-md shadow-amber-700 transition-all  cursor-pointer">
+          <button className="px-2 hover:bg-gray-950 text-white rounded-lg bg-gray-900 shadow-md shadow-amber-700 transition-all  cursor-pointer h-10">
             Iniciar sesión
           </button>
         </SignInButton>
       </SignedOut>
 
       <SignedIn>
-        <div className="flex justify-center mt-2 ring-2 ring-amber-700 rounded-full h-7">
+        <div className="flex justify-center items-center ring-2 ring-amber-700 rounded-full h-7">
           <UserButton />
         </div>
       </SignedIn>
